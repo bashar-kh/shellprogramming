b9349607ca0fdfad5b0f0b185c9bb7a9cc47b8ca
Teilnehmer <bashar.alkhateeb@gmail.com>
2017-05-31 13:49:52 +0200

    use options with extra prameters in our scribt

--- getopttest.sh
+++ getopttest.sh
@@ -8,7 +8,22 @@ eval set -- $tmp
 
 for p in "$@"; do echo "nachher $p"; done
 
-echo hallo
+while :
+do
+	opt=$1;shift
+	case "$opt" in
+		-h|--help) 	echo "Usage: pipapo scribt will be exited"
+			   	exit 0;;
+		-d|--debug) 	debug=$1; shift
+				echo "debug mode $debug activated"
+			  	;;
+		-f|--foo) 	foo=1
+				echo "foo is 1"
+				;;
+		--)		break
+				;;
+	esac
+done
 
-
+echo "left: $@"
 
